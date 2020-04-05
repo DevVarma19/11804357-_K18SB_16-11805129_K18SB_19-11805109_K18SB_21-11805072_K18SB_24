f230af9f0ab1d893c59122d5fd2ffa70dbaacaa1
DevVarma19 <varma.dev.01@gmail.com>
2020-04-05 08:59:20 +0530

    completed Demo

--- Demo_Notebook.py
+++ Demo_Notebook.py
@@ -64,7 +64,7 @@ y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
 
 X_train["Name"].value_counts()
 make_train = X_train["Name"].str.split(" ", expand=True)
@@ -155,12 +155,7 @@ from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = y_train.reshape(-1, 1)
-y_train = sc_y.fit_transform(y_train)
 
-X_train.sum()
-y_train.sum()
 #Fitting simple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
@@ -168,15 +163,9 @@ regressor.fit(X_train, y_train)
 #predicting the Test set results 
 y_pred = regressor.predict(X_test)
 
-from sklearn.model_selection import cross_val_score
-scores = cross_val_score(LinearRegression(), X_train, y_train, cv=5, scoring='r2')
-print(scores)
-
 from sklearn.metrics import r2_score
 r2_score(y_test, y_pred)
-'''
-    Output :- 0.730265013874368
-'''
+
 from sklearn.ensemble import RandomForestRegressor
 rf = RandomForestRegressor(n_estimators = 100)
 rf.fit(X_train, y_train.ravel())
@@ -184,6 +173,6 @@ y_pred = rf.predict(X_test)
 r2_score(y_test, y_pred)
 
 
-
-
-
+'''
+    Output : 0.8104532069495102
+'''
